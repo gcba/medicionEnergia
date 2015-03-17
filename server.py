708db67131a0f7635d2ea001067799107063a3f4
Pili Mayora <pili.mayora@gmail.com>
2015-03-17 17:56:21 -0300

    Actualizo sensores reales y muestro el valor en la pantalla

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ class consumoEnergetico(BaseNamespace, BroadcastMixin):
     def recv_connect(self):
         def sendapi():
             r = []
-            borneras = ["9061", "9062", "9063", "9064", "9065", "9066", "9071", "9072", "9073", "9074", "9075"]
+            borneras = ["9061", "9062", "9063", "9013", "9015", "9071", "9072", "9073", "9074", "9075", "9014", "9016"]
             while True:
                 for i in borneras:
                     response = urllib.urlopen("http://52.10.233.24/v1/circuits/{0}/latest".format(i))
