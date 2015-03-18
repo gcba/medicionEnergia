68965e777a523deca7ffb97f268b2d157842d8e7
lmokto <lm.uranga@gmail.com>
2015-03-18 16:49:15 -0300

    Incorporo funcion para consumir datos del clima en BA

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from gevent import monkey; monkey.patch_all()
 import gevent
-
+import time
 import random
 import urllib
 import json
@@ -10,22 +10,33 @@ from socketio.server import SocketIOServer
 from socketio.namespace import BaseNamespace
 from socketio.mixins import BroadcastMixin
 
+def clima():
+    res = urllib.urlopen("http://api.openweathermap.org/data/2.5/weather?id=3435910")
+    rclima = json.load(res)
+    estado = rclima['weather'][0]['description'].lower().encode()
+    return estado
 
 class consumoEnergetico(BaseNamespace, BroadcastMixin):
     def recv_connect(self):
         def sendapi():
             r = []
+            restado = clima()
+            count = 0
             borneras = ["9061", "9062", "9063", "9013", "9015", "9071", "9072", "9073", "9074", "9075", "9014", "9016"]
             while True:
+                count += 1
+                if count == 3500000:
+                    restado = clima()
+                    count = 0
                 for i in borneras:
                     response = urllib.urlopen("http://52.10.233.24/v1/circuits/{0}/latest".format(i))
                     result = json.load(response)
-                    print result['data'][0]['proc']
+                    #print result['data'][0]['proc']
                     r.append(result['data'][0]['proc']["power"])
                 percent = sum(r)
-                print percent
+                #print percent
                 r=[]
-                self.emit('consumo_total', {'power': percent})
+                self.emit('consumo_total', {'power': percent, "clima": restado})
             gevent.sleep(0.1)
         self.spawn(sendapi)
 
