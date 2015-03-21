ca5958df07b90a6e61d4fe2021ab20b82f50771a
lmokto <lm.uranga@gmail.com>
2015-03-20 22:35:41 -0300

    haciendo server a prueba de todo

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
-from gevent import monkey; monkey.patch_all()
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from gevent import monkey
+monkey.patch_all()
 import gevent
 import time
 import random
@@ -12,27 +16,29 @@ from socketio.namespace import BaseNamespace
 from socketio.mixins import BroadcastMixin
 import time
 
-# traducir 
+# traducir
 cielo = {
-    800 : "Soleado",
-    801 : "Parcialmente soleado",
-    802 : "Parcialmente nublado",
-    803 : "Mayormente nublado",
-    804 : "Nublado"
+    800: "Soleado",
+    801: "Parcialmente soleado",
+    802: "Parcialmente nublado",
+    803: "Mayormente nublado",
+    804: "Nublado"
 }
 
 httplib.HTTPConnection.debuglevel = 0
 
 try:
-    ip   = os.environ['OPENSHIFT_PYTHON_IP']
+    ip = os.environ['OPENSHIFT_PYTHON_IP']
     port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
 except:
     ip = "0.0.0.0"
-    port = "8080"
+    port = 8080
+
 
 def clima():
     try:
-        res = urllib.urlopen("http://api.openweathermap.org/data/2.5/weather?id=3435910")
+        res = urllib.urlopen(
+            "http://api.openweathermap.org/data/2.5/weather?id=3435910")
         rclima = json.load(res)
         clima = rclima['main']
         clima.update({"cielo": cielo[rclima['weather'][0]['id']]})
@@ -40,26 +46,37 @@ def clima():
     except:
         return "null"
 
+
 def GET(path):
     try:
-        response = urllib.urlopen("http://52.10.233.24/v1/circuits/{0}/latest".format(path))
+        result = []
+        response = urllib.urlopen(
+            "http://52.10.233.24/v1/circuits/{0}/latest".format(path))
         result = json.load(response)
-        return result
+        if hasattr(result, "data"):
+            print result
+            return result['data'][0]['proc']["power"]
+        else:
+            return 0
     except:
         return 0
 
+
 class consumoEnergetico(BaseNamespace, BroadcastMixin):
+
     def recv_connect(self):
         def sendapi():
-            r_aire = []
-            r_luz = []
-            r_tomas = []
+
+            r_luz, r_aire, r_tomas = [], [], []
+            suma_aire, suma_luz, suma_tomas, suma_total = 0, 0, 0, 0
 
             restado = clima()
             count = 0
+
             borneras_aire = ["9061", "9062", "9063"]
             borneras_luz = ["9014", "9016"]
-            borneras_tomas = ["9013", "9015", "9071", "9072", "9073", "9074", "9075"]
+            borneras_tomas = [
+                "9013", "9015", "9071", "9072", "9073", "9074", "9075"]
 
             while True:
                 count += 1
@@ -67,44 +84,42 @@ class consumoEnergetico(BaseNamespace, BroadcastMixin):
                 if count == 360:
                     restado = clima()
                     count = 0
-                
+
                 # loopear por borneras de aire
                 for i in borneras_aire:
-                    result = GET(i)
-                    r_aire.append(result['data'][0]['proc']["power"])
+                    power = GET(i)
+                    r_aire.append(power)
 
                 # loopear por borneras de luz
                 for i in borneras_luz:
-                    result = GET(i)
-                    r_luz.append(result['data'][0]['proc']["power"])
+                    power = GET(i)
+                    r_luz.append(power)
 
                 # loopear por borneras de tomas
                 for i in borneras_tomas:
-                    result = GET(i)
-                    r_tomas.append(result['data'][0]['proc']["power"])
+                    power = GET(i)
+                    r_tomas.append(power)
 
                 suma_aire = sum(r_aire)
                 suma_luz = sum(r_luz)
                 suma_tomas = sum(r_tomas)
                 suma_total = suma_aire + suma_luz + suma_tomas
-                
+
                 # reset
                 r_aire = []
                 r_luz = []
                 r_tomas = []
-                
-                try:
-                    self.emit('consumo_total', {'power_total': suma_total, 'power_aire': suma_aire, \
-                        'power_luz': suma_luz, 'power_tomas': suma_tomas, 'clima': restado})
-                except:
-                    self.emit('consumo_total', {})
-                time.sleep(5)
+
+                self.emit('consumo_total', {'power_total': suma_total, 'power_aire': suma_aire,
+                                            'power_luz': suma_luz, 'power_tomas': suma_tomas, 'clima': restado})
+                time.sleep(1)
 
             gevent.sleep(0.1)
         self.spawn(sendapi)
 
 
 class Application(object):
+
     def __init__(self):
         self.buffer = []
 
@@ -121,8 +136,6 @@ class Application(object):
                 content_type = "text/javascript"
             elif path.endswith(".css"):
                 content_type = "text/css"
-            # elif path.endswith(".svg"):
-            #     content_type = "application/x-shockwave-flash"
             else:
                 content_type = "text/html"
 
@@ -142,8 +155,5 @@ def not_found(start_response):
 
 
 if __name__ == '__main__':
-    print 'Listening on port {0} ip {1}'.format("8080", ip)
-    try:
-        SocketIOServer((ip, 8080), Application(), resource="socket.io").serve_forever()
-    except:
-        SocketIOServer((ip, 8080), Application(), resource="socket.io").serve_forever()
+    print 'Listening on port {0} ip {1}'.format(ip, port)
+    SocketIOServer((ip, port), Application(), resource="socket.io").serve_forever()
